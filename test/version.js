c14cd3c069ca55571759b5b70f03a60f9891d3eb
Anson Yu Wang <opensource@undozen.com>
2015-03-21 16:07:16 +0800

    mv test/stop.js to test/version.js

--- test/version.js
+++ test/version.js
@@ -2,10 +2,10 @@
 global.Promise = require('bluebird');
 var tape = require('tape');
 var co = require('co');
+var utils = require('./utils');
 
 tape('get server version', co.wrap(function * (test) {
-    var utils = require('./utils');
-    test.plan(3);
+    test.plan(2);
     yield utils.ready;
     var buf = new Buffer(0);
     var socket = yield utils.connect(28692);
@@ -14,5 +14,14 @@ tape('get server version', co.wrap(function * (test) {
     test.equal(typeof obj.version, 'string');
     console.log(obj);
     test.ok(obj.version.match(/^\d+\.\d+\.\d+/));
-    utils.stop(test);
+}));
+
+tape('server could be stopped', co.wrap(function * (test) {
+    test.plan(1);
+    yield utils.ready;
+    var buf = new Buffer(0);
+    var socket = yield utils.connect(28692);
+    process.nextTick(socket.end.bind(socket, '{"cmd":"stop"}'));
+    var obj = yield utils.respond(socket);
+    test.ok(obj.stopped);
 }));
